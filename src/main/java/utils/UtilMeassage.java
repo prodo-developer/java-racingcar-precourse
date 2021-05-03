58a1f14d00b2447a54902967828672b9fb278758
prodo-developer <wjdals300@gmail.com>
2021-05-04 04:20:02 +0900

    feat(UtilMeassage): 자동차 경주게임 공통 에러 메시지 구현

---
+++ src/main/java/utils/UtilMeassage.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class UtilMeassage {
+
+    public static final String RacingTryCountsNumberFormatException = "[ERROR] 시도 횟수는 1 이상의 숫자만 가능합니다.";
+    public static final String RandomMovingNumberRangeException  = "[ERROR] 랜덤 이동을 결정하는 랜덤값은 0 이상 9 이하여야 합니다. (값 : %d)";
+    public static final String CannotPlayGameException  = "[ERROR] 시도 횟수를 초과했기 때문에 게임을 진행할 수 없습니다.";
+    public static final String CannotFindWinnerCarException = "[ERROR] 우승 차량을 찾을 수 없습니다.";
+    public static final String DUPLICATION_ERROR_MESSAGE = "[ERROR] 자동차 이름들 중 중복이 존재해서는 안됩니다.";
+    public static final String INVALID_LENGTH_ERROR_MESSAGE = "[ERROR] 자동차 이름의 길이는 1자 이상 5자 이하여야 합니다.";
+    public static final String NULL_ERROR_MESSAGE = "[ERROR] Null값은 입력할 수 없습니다.";
+    public static final String BLANK_ERROR_MESSAGE = "[ERROR] 공백은 입력할 수 없습니다.";
+
+}
